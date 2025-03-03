7899234152df4c10fc1edd484117a87d8a956843
jingxinwang <wjxalexander@hotmail.com>
2025-03-04 02:10:58 +0800

    install neovim

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 chmod +x run.sh
+./run.sh -- te 
+./run.sh --dryrun
+
+./run.sh foo
--- runs/test.sh
+++ runs/test.sh
@@ -1,3 +1,10 @@
  #!/usr/bin/env bash
 
 echo "I am test"
+
+# this command is downloading Neovim version 0.10.1 source code from GitHub and placing it in the ~/personal/neovim directory on your system. 
+git clone -b v0.10.1 git@github.com:neovim/neovim.git $HOME/personal/neovim 
+cd $HOME/personal/neovim
+sudo apt install cmake gettext lua5.1 liblua5.1-0-dev
+make CMAKE_BUILD_TYPE=RelWithDebInfo #cmake
+sudo make install # install neovim the The Makefile comes from the Neovim source code repository that you cloned from GitHub

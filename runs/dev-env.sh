afe72cd986074645fcf5bffe80a61174a1b4563f
jingxinwang <wjxalexander@hotmail.com>
2025-03-05 03:26:43 +0800

    env files

---
+++ runs/dev-env.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+script_dir="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
+dry="0"
+
+while [[ $# > 0 ]]; do
+    if [[ "$1" == "--dry" ]]; then
+        dry="1"
+    fi
+    shift
+done
+
+log() {
+    if [[ $dry == "1" ]]; then
+        echo "[DRY_RUN]: $@"
+    else
+        echo "$@"
+    fi
+}
+
+execute() {
+    log "execute: $@"
+    if [[ $dry == "1" ]]; then
+        return
+    fi
+    "$@"
+}
+
+log "--------- dev-env ---------"
+cd $script_dir
+copy_dir() {
+    pushd $1
+    to=$2
+    dirs=$(find . -maxdepth 1 -mindepth 1 -type d)
+    for dir in $dirs; do
+        execute rm -rf $to/$dir
+        execute cp -r $dir $to/$dir
+    done
+    popd
+}
+
+copy_dir .config $XDG_CONFIG_HOME
